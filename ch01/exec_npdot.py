c7defa29dc61ff34be82e8dde2da234e8b412bee
Nelca <m.n.93710167@gmail.com>
2017-01-31 19:42:20 +0900

    check np.dot error on buw

--- ch01/exec_npdot.py
+++ ch01/exec_npdot.py
@@ -5,11 +5,16 @@ import pdb
 a = np.array([1, 2, 3])
 b = np.array([[4, 5], [4, 5], [4, 5]])
 
+c = np.random.randn(63,)
+d = np.random.randn(63,5)
 shape_a = a.shape
 shape_b = b.shape
 
 pdb.set_trace()
 
 result = np.dot(a, b)
+result_2 = np.dot(c, d)
 
+print(result)
+print(result_2)
 
