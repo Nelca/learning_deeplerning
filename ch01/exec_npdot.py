78be54889ffe545d64749d5eb7892ac65a36d730
Nelca <m.n.93710167@gmail.com>
2017-02-03 22:08:09 +0900

    test np dot

--- ch01/exec_npdot.py
+++ ch01/exec_npdot.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pdb
 
+def testFunc(np_array):
+    shape = np_array.shape
+    print(shape)
+
 a = np.random.randn(100,)
 b = np.random.randn(100,5)
+testFunc(a)
 
+pdb.set_trace()
+
+np.dot(a, b)
 result = np.dot(a, b)
 
 print(result)
