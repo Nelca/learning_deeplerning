fff31ccaa09eb0713ebaf64f25327c69a89843cd
Nelca <m.n.93710167@gmail.com>
2017-02-08 22:21:34 +0900

    add exercise script

---
+++ exercise/hello_python.py
@@ -0,0 +1,20 @@
+print("hello python")
+print("let's type print(\"hello()\")")
+
+def hello(name=""):
+    if name == "":
+        print("Hello someone!!")
+        print("So next step is type this-> hello(\"your name\")")
+    else:
+        print("Hello" + name + "!!")
+        print("OK. Next step")
+        print("Type ")
+
+
+
+
+
+
+
+
+#code.InteractiveConsole(globals()).interact()
