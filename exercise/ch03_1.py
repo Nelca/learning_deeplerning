8c5facdd172a5f4bb33c53aed0cfd5896e3e7b43
Nelca <m.n.93710167@gmail.com>
2017-03-13 13:55:33 +0900

    modify chapter 3-1 files

--- exercise/ch03_1.py
+++ exercise/ch03_1.py
@@ -3,40 +3,74 @@ import customconsole as cc
 
 class CustomConsole(cc.CustomConsole):
     def check_input(self, ri):
-        if ri == "hint_sigmoid":
+        replaced_ri = ri.replace(" ", "")
+        if replaced_ri == "a=np.array([1,2])":
             print("")
-            print("Sigmoid function is defined as follows.")
+            print("Good!!")
             print("")
-            print("1 / (1 + np.exp(-x))")
+            print("Then let's display the values of variable 'a'.")
+            print("Type this->")
+            print("print(a)")
             print("")
+        elif replaced_ri == "print(a)":
+            self.runsource(ri)
+            ri = ""
             print("")
-        elif ri == "hint_step_func":
+            print("Nice!")
+            print("The results are as described above.")
             print("")
-            print("Step function is this->")
+            print("Next is check dimention of 'a'")
+            print("Type this -> ")
+            print("np.ndmin(a)")
             print("")
-            print("def step_function(x):")
-            print("    return np.array(x > 0, dtype=np.int)")
+        elif replaced_ri == "np.ndmin(a)":
+            self.runsource(ri)
+            ri = ""
             print("")
-        elif ri == "hint_ReLU":
+            print("That's it!")
+            print("The results are as described above.")
             print("")
-            print("ReLU is this->")
+            print("Next let's display the number of elements of the array.")
+            print("Type this ->")
+            print("a.shape")
             print("")
-            print("def ReLU(x):")
-            print("    return np.maximum(0, x)")
+        elif replaced_ri == "a.shape":
+            self.runsource(ri)
+            ri = ""
             print("")
-        elif ri == "sig_a = np.array([1, 2, 3])":
-            print("That's it.")
-            print("So next type this-> sig_a_exp = np.exp(sig_a_exp)")
-        elif ri == "sig_a_exp = np.exp(sig_a_exp)":
-            print("Good good good.")
-            print("print(a_exp)")
-        elif ri == "y":
-            print("Is displayed this ? ")
-            print("array([False, True, True], dtype=bool)")
-            print("So, let's next chapter.")
-            print("type this -> nextCh()")
+            print("OK!")
+            print("This is a sigle dimention array results.")
+            print("")
+            print("Well, next we define a multidimensional array.")
+            print("Type this->")
+            print("b = np.array([[1, 2], [3, 4]])")
+            print("")
+        elif replaced_ri == "b=np.array([[1,2],[3,4]])":
+            self.runsource(ri)
+            ri = ""
+            print("")
+            print("Okay!")
+            print("So let's check variable 'b'.")
+            print("")
+            print("Type this->")
+            print("print(b)")
+            print("np.ndmin(b)")
+            print("b.shape")
+            print("")
+        elif replaced_ri == "b.shape":
+            self.runsource(ri)
+            ri = ""
+            print("")
+            print("Gooood!!!")
+            print("Next exercise will do the dot product of matrix.")
+            print("")
+            print("Define the new array as follow.")
+            print("c = np.array([[1, 2, 3], [4, 5, 6]])")
+            print("")
+        elif ri == "hint":
+            print("")
         return ri
 
-ch03_console = CustomConsole(question_file= "./ch03_functions.py")
-ch03_console.interact("### welcome chapter 3 !!! ###")
+ch03_1_console = CustomConsole(question_file= "./ch03_1_functions.py")
+ch03_1_console.interact("### welcome chapter 3-1 !!! ###")
 
--- exercise/ch03_1_functions.py
+++ exercise/ch03_1_functions.py
@@ -1,96 +1,21 @@
 import numpy as np
 
-hint_sigmoid = ""
-hint_step_func = ""
-hint_ReLU = ""
+def printInitialMessage():
+    print("")
+    print("*******************************")
+    print("")
+    print("This section lerning about computing multidimentional arrays.")
+    print("")
+    print("First step is define numpy array as follows.")
+    print("")
+    print("a = np.array([1, 2, 3, 4])")
+    print("")
 
-print("")
-print("")
-print("")
-print("Hello. This section is lerning neural netowok.")
-print("")
-print("First of all, lerning a activation function.")
-print("")
-print("Let's define 'sigmoid' function.")
-print("")
-print("If you want hint, type this ->")
-print("hint_sigmoid")
-print("")
-print("")
-print("and type this->checkSigmoid()")
-
-def collect_sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def collect_step_function(x):
-    return np.array(x > 0, dtype=np.int)
-
-def collect_ReLU(x):
-    return np.maximum(0, x)
-
-def checkSigmoid():
-    chk_num = np.array([-1.0, 0.5, 2.1])
-    result = sigmoid(chk_num)
-    collect_result = collect_sigmoid(chk_num)
-    if all(result == collect_result):
-        print("")
-        print("OK!!!")
-        print("Sigmoid is collect.")
-        print("")
-        print("Next activatioin function is 'step function'.")
-        print("Define the 'step_function', and type checkStepFunction()")
-        print("If you want hint, type this -> hint_step_func")
-        print("")
-        print("")
-    else:
-        print("")
-        print("Mmmmm define function is not collect.")
-        print("Check the function name as 'sigmoid'")
-        print("If you want hint, type this -> hint_sigmoid")
-        print("")
-
-def checkStepFunction():
-    chk_num = np.array([-0.5, 0, 1.0])
-    result = step_function(chk_num)
-    collect_result = collect_step_function(chk_num)
-    if all(result == collect_result):
-        print("")
-        print("Nice!!")
-        print("Step function is define collect.")
-        print("")
-        print("Next activatioin function is Rectified Liner Unit as ReLU.")
-        print("Define the 'ReLU', and type checkReLU()")
-        print("If you want hint, type this -> hint_ReLU")
-        print("")
-    else:
-        print("")
-        print("Mmmmm define function is not collect.")
-        print("Check the function name as 'step_function'")
-        print("If you want hint, type this -> hint_step_func")
-        print("")
-
-def checkReLU():
-    chk_num = np.array([-0.5, 0, 1.0, 1.4])
-    result = ReLU(chk_num)
-    collect_result = collect_ReLU(chk_num)
-    if all(result == collect_result):
-        print("")
-        print("That's greate!!!")
-        print("ReLU function is collect.")
-        print("")
-        print("So next step is lerning about computing multidimentional arrays.")
-        nextChapter()
-    else:
-        print("")
-        print("Mmmmm define function is not collect.")
-        print("Check the function name as 'ReLU'")
-        print("If you want hint, type this -> hint_ReLU")
-        print("")
-
-
-def nextChapter(file_name="ch03_1.py"):
+def nextChapter(file_name="ch03_2.py"):
     with open(file_name) as next_chapter:
         next_code = next_chapter.read()
         exec(next_code)
 
+
+printInitialMessage()
 
