59142bc3694158b5e2d6f100b4823bd093001345
Nelca <m.n.93710167@gmail.com>
2017-04-25 09:27:37 +0900

    modify chapter5

--- exercise/ch05_3.py
+++ exercise/ch05_3.py
@@ -4,8 +4,8 @@ import customconsole as cc
 class CustomConsole(cc.CustomConsole):
     def check_input(self, ri):
        replaced_ri = ri.replace(" ", "")
-       if ri == "hint_ml_forward":
-            self.runsource("print(hint_ml_forward)")
+       if ri == "hint_network":
+            self.runsource("print(hint_network)")
             ri = ""
        elif replaced_ri == "hint_ml_backward":
             self.runsource("print(hint_ml_backward)")
--- exercise/ch05_3_functions.py
+++ exercise/ch05_3_functions.py
@@ -17,35 +17,53 @@ train_loss_list = []
 train_acc_list = []
 test_acc_list = []
 
+ans_network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+hint_network = """
+The network answer is as follow.
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+And check your answer as follow.
+checkNet()
+"""
+
 ##########   inital message   ##############
 print("*****************************")
 print("")
 print("This chpater is train neural net.")
 print("Let's define the training .")
+print("")
+print("First of all, define the network as 'network'.")
+print("Network initial argument are as follow.")
+print("input_size=784, hidden_size=50, output_size=10")
+print("")
+print("And check your network as follow.")
+print("checkNet()")
 
-def checkMLForward():
-    chk_x = 2
-    chk_y = 3
-    correct_ans = ans_mul_layer.forward(chk_x, chk_y)
-    chk_ans = mul_layer.forward(chk_x, chk_y)
-    if correct_ans==chk_ans:
-        print("")
+def checkNet():
+    chk_1 = ans_network==network
+    if chk_1:
         print("Goood!!!!")
         print("")
-        print("Next step is backward.")
-        print("Let's define backward function like forward function.")
+        print("Next step is calculate epoch.")
+        print("Training size and batch size are defined as follows.")
         print("")
-        print("And check defined answers as follow.")
-        print("checkMLBackward()")
+        print("train_size = x_train.shape[0]")
+        print("batch_size = 100")
+        print("")
+        print("Let's define the epoch as 'iter_per_epoch'")
+        print("")
+        print("And check your answer as follow.")
+        print("checkEpoch()")
     else:
-        print("")
         print("Ooops, your answer is incorrect.")
         print("")
         print("If you want a hint type this->")
-        print("hint_ml_forward")
+        print("hint_network")
         print("")
         print("And check defined answers as follow.")
-        print("checkMLForward()")
+        print("checkNet()")
 
 def nextChapter(file_name="ch06.py"):
     with open(file_name) as next_chapter:
