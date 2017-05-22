3b7d3e816d68db273b96f168e493edb940d0192e
Nelca <m.n.93710167@gmail.com>
2017-05-22 12:39:06 +0900

    modify chapter 8

--- ch08/train_deepnet.py
+++ ch08/train_deepnet.py
@@ -2,7 +2,6 @@
 import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
-import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 from deep_convnet import DeepConvNet
 from common.trainer import Trainer
---
+++ ch08/use_cnn.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
+from deep_convnet import DeepConvNet
+from dataset.mnist import load_mnist
+import pdb
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+
+pdb.set_trace()
+
+network = DeepConvNet()
+network.load_params("deep_convnet_params.pkl")
+
+print("calculating test accuracy ... ")
+#sampled = 1000
+#x_test = x_test[:sampled]
+#t_test = t_test[:sampled]
+
+classified_ids = []
+
+acc = 0.0
+batch_size = 100
+
+for i in range(int(x_test.shape[0] / batch_size)):
+    tx = x_test[i*batch_size:(i+1)*batch_size]
+    tt = t_test[i*batch_size:(i+1)*batch_size]
+    y = network.predict(tx, train_flg=False)
+    print("predicting... " + str(i))
+    y = np.argmax(y, axis=1)
+    classified_ids.append(y)
+    acc += np.sum(y == tt)
+    
+acc = acc / x_test.shape[0]
+print("test accuracy:" + str(acc))
